cb6c0d0be60a4c85c3f2d4300a84f43344be3ad1
CLAUDIO PAGNINI <claudio.pagnini@ratpdev.com>
2023-04-03 11:28:26 +0200

    Added rotation
    Still have to improve

--- EXIF.py
+++ EXIF.py
@@ -1,7 +1,8 @@
 import sys
-from PIL import Image, ImageQt, ExifTags
+from PIL import Image,  ExifTags
+import PIL
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QTransform
 from PyQt5.QtWidgets import QApplication, QFileDialog, QLabel, QVBoxLayout, QWidget, QPushButton, QScrollArea, QSizePolicy
 
 
@@ -9,46 +10,64 @@ class ExifViewer(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle('EXIF Viewer')
-        self.setMinimumSize(640, 480)
+        self.setMinimumSize(1, 1)
+        self.setMaximumSize(512,512)
 
         # Add a button to open an image file
         self.open_button = QPushButton('Open Image', self)
         self.open_button.clicked.connect(self.open_image)
+        self.open_button = QPushButton('Rotate', self)
+        self.open_button.clicked.connect(self.rotate_image)
 
         # Add a label to display the image
         self.image_label = QLabel(self)
         self.image_label.setScaledContents(True)
-        self.image_label.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        
 
         # Add a label to display the EXIF data
         # scroll area widget - Labels will move inside this widget
         self.scroll_area = QScrollArea(self)
+        self.scroll_area.setMinimumSize(300, 100)
+        
         self.exif_label = QLabel('No image opened.',  self.scroll_area)
+        self.exif_label.setSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        #self.exif_label.setMinimumSize(580, 380)
         self.scroll_area.setWidget(self.exif_label)
         self.scroll_area.setWidgetResizable(True)
 
         # Set up the main layout
         self.layout = QVBoxLayout()
+        self.layout.insertWidget(0,self.open_button)
         self.layout.addWidget(self.open_button)
         self.layout.addWidget(self.image_label)
         self.layout.addWidget(self.scroll_area)
         self.setLayout(self.layout)
+    
+
+    def rotate_image(self):
+        img = QPixmap(file_path)
+        trans = QTransform()
+        trans.rotate(90)
+        self.image_label.setPixmap(img.transformed(trans))
         
+
     def open_image(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        global file_path
         # Open an image file and extract its EXIF data
         file_path, _ = QFileDialog.getOpenFileName(
-            self, 'Open Image', '', 'Image Files (*.jpg *.png *.bmp)')
+            self, 'Open Image', '', "Image Files (*.jpg *.png *.jpeg *.bmp *.gif)", options=options)
         if file_path:
             try:
                 with Image.open(file_path) as img:
-                    # Scale the image to fit in the window
-                    img.thumbnail((320, 320))
-
                     # Display the image
-                    qimg = ImageQt.toqpixmap(img)
+                    qimg = QPixmap(file_path)
                     self.image_label.setPixmap(qimg)
 
+                    
                     exif_data = img._getexif()
+                    
                     if exif_data:
                         # Format the EXIF data as a string
                         exif_str = ''
@@ -59,8 +78,8 @@ class ExifViewer(QWidget):
                                 try:
                                     tag_value = tag_value.decode('utf-8')
                                 except UnicodeDecodeError:
-                                    tag_value = tag_value.decode(
-                                        'utf-8', 'replace')
+                                    tag_value = tag_value.decode('utf-8', 'replace')
+
                             exif_str += f'{tag_name}: {tag_value}\n'
 
                         # Set the label's text to the formatted EXIF data
@@ -76,4 +95,4 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = ExifViewer()
     window.show()
-    sys.exit(app.exec_())
+    sys.exit(app.exec())
